214203d87a5fa29873db47af351ce29e2aa96f85
Aravinth PM <110657801+aravinthpm77@users.noreply.github.com>
2024-03-14 20:57:35 +0530

    Styling Navbar

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -10,20 +10,19 @@ const Navbar=()=> {
     
 
   return (
-    <div className=' max-auto px-40 py-5 flex justify-between bg-stone-200'>
+    <div className=' max-auto px-20 py-5 flex justify-around items-center  text-center bg-stone-200'>
         <div className='flex'>
             <Link to='/' className='logo'>
                 <img width="50" height="50" src="https://assets-global.website-files.com/6369a6904656314914c2bae2/6369a8d295be04a841e17ea5_logo.svg" alt="bookmark-ribbon--v1"/>    
             </Link>
             
-        </div>
-        
+        </div >
         <Link to='/' className=''>
                 <p onClick={toggleDropdown}>Books</p>
         </Link>
         {isDropdownOpen && (
-                <div className="absolute mx-60">
-                    <div className='flex bg-slate-50'>
+                <div className="absolute ">
+                    <div className='flex bg-slate-300 rounded-xl my-70'>
                         <div className='m-10 grid gap-1'>
                             <h5 className='text-4xl'>EXPLORE</h5>
                             <Link to='/'>Bestsellers</Link>
@@ -39,8 +38,8 @@ const Navbar=()=> {
                             <Link to='/'>Membership</Link>
                             
                         </div>
-                        <div className='grid'>
-                            <img width="200" height="300" src="https://blmparis.files.wordpress.com/2014/07/day.jpg" alt="image1"/>
+                        <div className='grid '>
+                            <img width="200" height="300" className='rounded-tr-2xl' src="https://blmparis.files.wordpress.com/2014/07/day.jpg" alt="image1"/>
                             <p>Book/Author Name</p>
                         </div>  
                     </div>
@@ -57,15 +56,17 @@ const Navbar=()=> {
         <Link to='/' className='cart'>
             <p>Authors</p>
         </Link>
+      
         
-        <div className='flex'>
+        
+        <div className='flex items-center'>
             <Link to='/' className='cart'>
-                <img width="30" height="30" src="https://img.icons8.com/fluency-systems-filled/48/search.png" alt="search"/>
+                <img width="24" height="24" src="https://img.icons8.com/fluency-systems-filled/48/search.png" alt="search"/>
             </Link>
             <Link to='/' className='cart'>
                 <img width="32" height="32" src="https://img.icons8.com/windows/32/buy.png" alt="buy"/>
             </Link>
-            <Link to='/' className='bg-stone-300 py-2 px-6 font-medium text-slate-00 hover:bg-stone-400 '>
+            <Link to='/' className='bg-stone-300 py-3 px-6   hover:bg-stone-400 rounded-lg '>
                 <button>Login</button>
             </Link>
         </div>
