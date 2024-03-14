77ac14b5b26c13e030d18f95a7b16fa2da5ef1c4
Aravinth PM <110657801+aravinthpm77@users.noreply.github.com>
2024-03-14 23:37:42 +0530

    Navbar - Styling

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -10,51 +10,49 @@ const Navbar=()=> {
     
 
   return (
-    <div className=' max-auto px-20 py-5 flex justify-around items-center  text-center bg-stone-200'>
-        <div className='flex'>
-            <Link to='/' className='logo'>
-                <img width="50" height="50" src="https://assets-global.website-files.com/6369a6904656314914c2bae2/6369a8d295be04a841e17ea5_logo.svg" alt="bookmark-ribbon--v1"/>    
+    <div className='  px-20 py-5 flex  justify-evenly items-center  text-center bg-stone-200'>
+        <div className='flex '>
+            <Link to='/' className='logo pr-60 cursor-auto'>
+                <img width="50" height="50" className='cursor-pointer' src="https://assets-global.website-files.com/6369a6904656314914c2bae2/6369a8d295be04a841e17ea5_logo.svg" alt="bookmark-ribbon--v1"/>    
             </Link>
             
         </div >
-        <Link to='/' className=''>
-                <p onClick={toggleDropdown}>Books</p>
-        </Link>
+        
+        <button onClick={toggleDropdown} className='mr-10 py-3 px-6  ml-2 transition-all duration-300 pointer-events-auto rounded-lg hover:bg-slate-50  cursor-pointer'>Books</button>
+        
         {isDropdownOpen && (
-                <div className="absolute mx-80 my-100;">
-                    <div className='flex bg-slate-50 rounded-3xl my-12 mx-50' >
-                    <div class='m-10 grid gap-0.5'>
-                        <h5 class='text-0.5xl font-semibold leading-6 text-gray-900'>EXPLORE</h5>
-                        <Link to='/' class='mt-2'>Bestsellers</Link>
-                        <Link to='/' class='mt-2'>New Releases</Link>
-                        <Link to='/' class='mt-2'>Popular Genres</Link>
-                        <div className=''>
-                            <div class='mt-20'>
-                                <button class=" hover:bg-orange-200 ...   rounded-lg mx-5">
-                                <p class='text-sm text-orange-600 mx-6 my-4'>Browse all </p>
-                                {/* <div>
-                                    <img width="16" height="16" src="https://img.icons8.com/small/16/forward-arrow.png" alt="forward-arrow"/>
-                                </div> */}
-                                </button>
-                            </div>  
+                <div className=" mx-120 my-100 ">
+                    <div className='absolute flex bg-slate-50 rounded-xl  border border-stone-300   left-40 my-10 mx-48 py-2  w-21 shadow-lg  scale-90'  >
+                        <div class='m-10 grid gap-0.5  '>
+                            <h5 class='text-0.5xl font-semibold leading-6 text-gray-900 text-left tracking-wider'>EXPLORE</h5>
+                            <Link to='/' class='mt-2 text-left tracking-wider'>Bestsellers</Link>
+                            <Link to='/' class='mt-2 text-left tracking-wider'>New Releases</Link>
+                            <Link to='/' class='mt-2 text-left tracking-wider'>Popular Genres</Link>
+                            <div className=''>
+                                <div class='mt-10  '>
+                                    <button class=" hover:bg-orange-200 ...   rounded-lg mx-5">
+                                    <p class='text-sm text-orange-600 mx-6 my-4 tracking-wider '>Browse all </p>
+                                    </button>
+                                </div>  
+                            </div>
                         </div>
-                    </div>
 
-                        <div className='dropdown-mid m-10 grid gap-1' >
-                            <h5 className='text-0.5xl  font-semibold leading-6 text-gray-900 mx-20'>LIBRARIES</h5>
-                            <Link to='/'>Browse</Link>
-                            <Link to='/'>Membership</Link>
-                            <Link to='/'>Accessibility</Link>
-                            <Link to='/'>Benefits</Link>
-                            <Link to='/'>Membership</Link>
+                        <div className='dropdown-mid m-10 grid gap-1 text-left mr-20' >
+                            <h5 className='text-left text-0.5xl   font-semibold  text-gray-900 tracking-wider '>LIBRARIES</h5>
+                            <Link to='/' className='text-left tracking-wider'>Browse</Link>
+                            <Link to='/' className='text-left tracking-wider'>Membership</Link>
+                            <Link to='/' className='text-left tracking-wider'>Accessibility</Link>
+                            <Link to='/' className='text-left tracking-wider'>Benefits</Link>
+                            <Link to='/' className='text-left tracking-wider'>Membership</Link>
                             
-                        </div>
-                        <div class='grid overflow-hidden my-3 mx-4 rounded-3xl'>
-                            <div class='bg-sky-400 hover:bg-sky-700 h-250 w-250'>
+                        </div> 
+                        <div class='grid overflow-hidden my-3 mx-4 rounded-xl '>
+                        
+                            <div class='bg-stone-200 transition-all duration-500  hover:bg-stone-300 hover:scale-105 h-250 w-250'>
                                 <div class='my-8 mx-1'>
-                                    <h2 class='text-white text-center'>FEATURED</h2>
-                                    <img width="150" height="150" class='mx-14 my-7   rounded-3xl hover:translate-x-0.5' src="https://blmparis.files.wordpress.com/2014/07/day.jpg" alt="image1"/>
-                                    <p class='text-center text-white'>Book/Author Name</p>
+                                <h2 class='text-gray-800 font-bold text-center tracking-wider'>FEATURED</h2>
+                                    <img width="150" height="150" class='mx-14 my-7 transition-all duration-500 scale-105  rounded-xl hover:scale-110' src="https://blmparis.files.wordpress.com/2014/07/day.jpg" alt="image1"/>
+                                    <p class='text-center text-gray-800 font-semibold tracking-wider'>Book/Author Name</p>
                                 </div>
 
                             </div>
@@ -66,26 +64,29 @@ const Navbar=()=> {
                 </div>
         )}
         
-        <Link to='/' className=''>
+        <Link to='/' className='mr-10 py-3 px-6  ml-2 transition-all duration-300 rounded-lg hover:bg-slate-50 '>
             <p>For Kids</p>
         </Link>
-        <Link to='/' className='cart'>
+        <Link to='/' className='mr-10 py-3 px-6 ml-2 transition-all duration-300 rounded-lg hover:bg-slate-50 '>
             <p>Genres</p>
         </Link>
-        <Link to='/' className='cart'>
+        <Link to='/' className='mr-10 py-3 px-6  ml-2 transition-all duration-300 rounded-lg hover:bg-slate-50 '>
             <p>Authors</p>
         </Link>
       
         
         
-        <div className='flex items-center'>
-            <Link to='/' className='cart'>
+        <div className='flex items-center '>
+            <Link to='/' className='mr-10 p-3 ml-2 transition-all duration-300 rounded-lg hover:bg-stone-300 '>
                 <img width="24" height="24" src="https://img.icons8.com/fluency-systems-filled/48/search.png" alt="search"/>
             </Link>
-            <Link to='/' className='cart'>
+            <div className='w-0.5 h-8 mr-3 bg-stone-400'>
+
+            </div>
+            <Link to='/' className='p-2 ml-2 mr-2 border border-stone-400  transition-all duration-300  rounded-lg hover:bg-stone-300 '>
                 <img width="32" height="32" src="https://img.icons8.com/windows/32/buy.png" alt="buy"/>
-            </Link>
-            <Link to='/' className='bg-stone-300 py-3 px-6   hover:bg-stone-400 rounded-lg '>
+            </Link> 
+            <Link to='/' className='bg-stone-300 h-12 py-3 px-6 transition-all duration-300   hover:bg-stone-400 rounded-lg '>
                 <button>Login</button>
             </Link>
         </div>
